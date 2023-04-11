536847e616ba49cf3e7d46b3618d103b0f6c000f
justina A <aluzuj@gmail.com>
2023-04-11 16:15:12 -0500

    new

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,10 @@
-# from flask import Flask
+from flask import Flask
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return 'I am now a DevOps Engineer and ready to get into the job market'
+@app.route('/')
+def index():
+    return 'I am now a DevOps Engineer and ready to get into the job market'
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
